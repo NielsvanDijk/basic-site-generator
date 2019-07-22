329ae13ef4d81a87294c09b227667182581debac
Niels van Dijk <info@vandijk-vormgeving.nl>
2019-07-23 00:10:43 +0200

    Cleaned the settings.scss and linter some stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,66 +3,71 @@
     Project Configuration for gulp tasks.
 */
 
-const path = require('path');
-const sassFiles             = 'src/scss/style.scss';
-const styleDestination      = 'dist/style';
+const path = require("path");
+const sassFiles = "src/scss/**/*.scss";
+const styleDestination = "dist/style";
 
 /**
     Load Plugins.
     Load gulp plugins and assing them semantic names.
  */
 
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
 
-const gutil      = require('gulp-util');
-const chalk      = require('chalk');
-// const minifycss  = require('gulp-minify-css');
+const gutil = require("gulp-util");
+const chalk = require("chalk");
 
-const { gulpSassError } = require('gulp-sass-error');
+const { gulpSassError } = require("gulp-sass-error");
 
 // CSS related plugins.
-var sass         = require('gulp-sass'); // Gulp pluign for Sass compilation
-var autoprefixer = require('gulp-autoprefixer'); // Autoprefixing magic
-var minifycss    = require('gulp-uglifycss'); // Minifies CSS files
+var sass = require("gulp-sass"); // Gulp pluign for Sass compilation
+var autoprefixer = require("gulp-autoprefixer"); // Autoprefixing magic
+var minifycss = require("gulp-uglifycss"); // Minifies CSS files
 
 // JS related plugins.
-var concat       = require('gulp-concat'); // Concatenates JS files
-var uglify       = require('gulp-uglify'); // Minifies JS files
+var concat = require("gulp-concat"); // Concatenates JS files
+var uglify = require("gulp-uglify"); // Minifies JS files
 
 // Utility related plugins.
-var rename       = require('gulp-rename'); // Renames files E.g. style.css -> style.min.css
-var sourcemaps   = require('gulp-sourcemaps'); // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css)
-var notify       = require('gulp-notify'); // Sends message notification to you
+var rename = require("gulp-rename"); // Renames files E.g. style.css -> style.min.css
+var sourcemaps = require("gulp-sourcemaps"); // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css)
+var notify = require("gulp-notify"); // Sends message notification to you
 
 /**
     Task: styles
     Compiles Sass, Autoprefixes it and Minifies CSS
 */
 
-gulp.task('styles', function(){
-    gulp.src( sassFiles )
-        .pipe(sourcemaps.init())
-        .pipe( sass({
-            errLogToConsole: true,
-            outputStyle: 'compressed', // compressed || nested || expanded
-            precision: 10
-        }).on('error', sass.logError))
-        .pipe(sourcemaps.write({includeContent: false}))
-        .pipe(sourcemaps.init({loadMaps: false}))
-        .pipe( autoprefixer(
-			'last 2 version',
-			'> 1%',
-			'safari 5',
-			'ie 8',
-			'ie 9',
-			'opera 12.1',
-			'ios 6',
-			'android 4'))
-        .pipe(sourcemaps.write (styleDestination))
-        .pipe(rename( { suffix: '.min' } ) )
-		.pipe( gulp.dest( styleDestination ) )
-		.pipe(notify( { message: 'TASK Completed!', onLast: true }))
+gulp.task("styles", function() {
+  gulp
+    .src(sassFiles)
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        errLogToConsole: true,
+        outputStyle: "compressed", // compressed || nested || expanded
+        precision: 10
+      }).on("error", sass.logError)
+    )
+    .pipe(sourcemaps.write({ includeContent: true }))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(
+      autoprefixer(
+        "last 2 version",
+        "> 1%",
+        "safari 5",
+        "ie 8",
+        "ie 9",
+        "opera 12.1",
+        "ios 6",
+        "android 4"
+      )
+    )
+    .pipe(sourcemaps.write(styleDestination))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest(styleDestination))
+    .pipe(notify({ message: "TASK Completed!", onLast: true }));
 });
 
 /**
@@ -70,22 +75,19 @@ gulp.task('styles', function(){
     Refreshes the browser for you
 */
 
-gulp.task('browser-sync', function(){
-    const files = [
-        '**/*.php',
-        '**/*.html'
-    ];
+gulp.task("browser-sync", function() {
+  const files = ["**/*.php", "**/*.html"];
 
-    browserSync.init(files, {
-        server: "./",
-        port: 3000,
-    })
+  browserSync.init(files, {
+    server: "./",
+    port: 3000
+  });
 });
 
-gulp.task('watch', function(){
-    gulp.watch('./dist/styles/*.css', ['styles']);
-    gulp.watch("./src/scss/**/*.scss", ['styles']);
-    gulp.watch('./dist/style/*.css').on('change', browserSync.reload);
+gulp.task("watch", function() {
+  // gulp.watch("./dist/styles/*.css", ["styles"]);
+  gulp.watch("./src/scss/**/*.scss", ["styles"]);
+  gulp.watch("./dist/style/*.css").on("change", browserSync.reload);
 });
 
 /**
@@ -93,4 +95,4 @@ gulp.task('watch', function(){
     Compiles Sass, Autoprefixes it and Minifies CSS
 */
 
-gulp.task('default', ['styles', 'browser-sync', 'watch']);
+gulp.task("default", ["styles", "browser-sync", "watch"]);
